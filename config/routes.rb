c78b199c0e4d6051976bb626fcf4f3894890784c
Patrick <ruomungai@gmail.com>
2022-10-13 16:22:06 +0300

    adding session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :comments, only: [:index, :show, :update, :create, :destroy]
   resources :photos,only: [:index, :show, :update, :create, :destroy]
+  resources :sessions, only: [:create]
   patch "/photos/:id/like", to: "photos#increment_likes"
 
   
